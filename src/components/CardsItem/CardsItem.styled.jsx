22949cf5061a30c3c3ba3dc44ad2019d87a20754
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-04-27 00:29:47 +0300

    deploy

--- src/components/CardsItem/CardsItem.styled.jsx
+++ src/components/CardsItem/CardsItem.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import boy from 'Images/boy.png';
-import picture from 'Images/picture.png';
-import rectangle from 'Images/rectangle.png';
+import boy from 'images/boy.png';
+import picture from 'images/picture.png';
+import rectangle from 'images/rectangle.png';
 
 export const Btn = styled.button`
   background: #ebd8ff;
